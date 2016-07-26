eb792e01d04a62d52a421848cd8562073b3412d3
Piperlw <piperlw@me.com>
2016-07-26 10:09:13 -0700

    Corrected Version

--- Test_Bluetooth.py
+++ Test_Bluetooth.py
@@ -1,9 +1,9 @@
 # Test if lists are the same
-def test_lists
+def test_lists():
   if set(OldList) == set(NewListCat) is False:
     print "test_lists: Error"
 
 # Test if Bluetooth-discovery2.py has finished its first iteration
-def test_first
-  if first = 2 is False: 
+def test_first():
+  if first == 2 is False: 
     print "test_first: Error"
